dd32a016beaff2947cad9dbb2f195735e29f04bb
CN132DAS <CN_132das@163.com>
2025-05-14 17:11:42 +0800

    用纯代码写gui
    part 1

--- Qt_groupwork/mainwindow.cpp
+++ Qt_groupwork/mainwindow.cpp
@@ -6,6 +6,37 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->resize(1100,700);
+    setWindowTitle("MindMap");
+    QIcon WindowIcon(":/assets/WindowIcon.png");
+    setWindowIcon(WindowIcon);
+
+    QMenuBar *menuBar_ = new QMenuBar(this);
+    this->setMenuBar(menuBar_);
+
+    QMenu* fileOp = new QMenu("文件",menuBar_);
+    QMenu* newFile = new QMenu("新建",this);
+    QMenu* openFile = new QMenu("打开",this);
+    QMenu* save = new QMenu("保存",this);
+    QMenu* saveAs = new QMenu("另存为",this);
+
+    menuBar_->addMenu(fileOp);
+    fileOp->addMenu(newFile);
+    fileOp->addMenu(openFile);
+    fileOp->addSeparator();
+    fileOp->addMenu(save);
+    fileOp->addMenu(saveAs);
+
+    QIcon newFile_QI (":/assets/new-file.svg");
+    QIcon openFile_QI(":/assets/file.svg");
+    QIcon save_QI(":/assets/save.svg");
+    QIcon saveAs_QI(":/assets/save-as.svg");
+
+    newFile->setIcon(newFile_QI);
+    openFile->setIcon(openFile_QI);
+    save->setIcon(save_QI);
+    saveAs->setIcon(saveAs_QI);
+
 }
 
 MainWindow::~MainWindow()
--- Qt_groupwork/mainwindow.h
+++ Qt_groupwork/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMenuBar>
+#include <QMenu>
+#include <QIcon>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -12,7 +15,6 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
